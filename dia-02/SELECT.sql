b12b903288b063d69d953479e7d716e2311885a3
agostinhodesena <agostinhodesena>
2023-06-03 13:53:22 +0000

    Aula "Olá mundo" do dia 2

---
+++ dia-02/SELECT.sql
@@ -0,0 +1,40 @@
+-- Databricks notebook source
+-- para usar textos, usar aspas simples:
+
+SELECT 'Olá mundo!'
+
+-- COMMAND ----------
+
+-- realizando operações matematicas:
+
+SELECT 2+2
+
+-- COMMAND ----------
+
+SELECT 2 * 97
+
+-- COMMAND ----------
+
+-- exponenciação:
+
+SELECT POW(10,2)
+
+-- COMMAND ----------
+
+-- resto de uma divisão:
+
+SELECT MOD(9,2)
+
+-- COMMAND ----------
+
+-- concatenando informações:
+
+SELECT 'Olá, ' || 'Guilherme'
+
+-- COMMAND ----------
+
+SELECT concat('Olá, ', 'Guilherme')
+
+-- COMMAND ----------
+
+
